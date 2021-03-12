3c904cdd1f7597c65d8a80e2bb72fcee1af734d1
manueljgranados <mjgranados@gmail.com>
2021-03-12 09:24:45 +0100

    Task #2

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 public class PetclinicApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(PetclinicApplication.class, args);
 	}
 
